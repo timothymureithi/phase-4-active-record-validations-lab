93ed31ef0aa44b675bf060ed425ec76b327f5187
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-22 18:05:48 +0300

    Done

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,15 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { :minimum => 250 }
+    validates :summary, length: { :maximum => 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+
+    validate :clickbait
+    
+    def clickbait
+        if title == "True Facts"
+            errors.add(:title, "This is clickbait!")
+        end
+    end
+
 end
